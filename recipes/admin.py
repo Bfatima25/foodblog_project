15a1e0995636c4439b9d28327a56b71b89731ed9
Bfatima25 <bushrafatima102@gmail.com>
2022-03-10 19:43:53 -0600

    created superuser and register models with admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Recipe
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'list_date', 'employer')
+    list_display_links = ('id', 'title')
+    list_filter = ('employer',)
+    search_fields = ('title', 'Ingredients', 'Directions', 'Specialdiet', 'Mealtype', 'preptime', 'totaltime')
+    list_per_page = 25
+admin.site.register(Recipe, RecipeAdmin)
+
