e283bb12ef81d62f055a32802d3059c873af3be0
Bfatima25 <bushrafatima102@gmail.com>
2022-03-13 14:26:08 -0500

    filtered the search form on home page

--- recipes/views.py
+++ recipes/views.py
@@ -21,9 +21,30 @@ def recipe(request, recipe_id):
     return render(request, 'recipes/recipe.html', context)
 
 def search(request):
+    queryset_list = Recipe.objects.order_by('-list_date')
+
+    # Keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(Directions__icontains=keywords)
+        
+    # Meal Type
+    if 'meal' in request.GET:
+        meal = request.GET['meal']
+        if meal:
+            queryset_list = queryset_list.filter(Mealtype__iexact=meal)
+    
+    # Special Diet
+    if 'diet' in request.GET:
+        diet = request.GET['diet']
+        if diet:
+            queryset_list = queryset_list.filter(Specialdiet__iexact=diet)
 
     context = {
-        'search': recipe
+        'search': recipe,
+        'recipes': queryset_list
     }
+    
 
     return render(request, 'recipes/search.html', context)
