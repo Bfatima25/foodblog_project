a3652ee468e34a838b7c9f43a92cae0b105bfc34
Bfatima25 <bushrafatima102@gmail.com>
2022-03-14 17:33:55 -0500

    static files updated

--- foodblog/settings.py
+++ foodblog/settings.py
@@ -126,6 +126,11 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'foodblog/static')
 ]
 
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Media Folder Settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
