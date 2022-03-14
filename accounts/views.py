9434ebb44ff95b0f17aebc686a7f0260e447ebf8
Bfatima25 <bushrafatima102@gmail.com>
2022-03-14 02:09:02 -0500

    worked on user registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
 def register(request):
     if request.method == 'POST':
-        messages.error(request, 'testing message')
-        return redirect('register')
+        # Get form values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # check if passwords match
+        if password == password2:
+            if User.objects.filter(username=username):
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email):
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(username=username, password=password, 
+                    email=email, first_name=first_name, last_name=last_name)
+
+                    auth.login(request, user)
+                    messages.success(request, 'You are now logged in')
+                    return redirect('index')
+        else:
+            messages.error(request, 'passwords do not match')
+            return redirect('register')
     else:
         return render(request, 'accounts/register.html')
 
